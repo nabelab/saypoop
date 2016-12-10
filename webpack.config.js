546be528d5f30d7cce5fa44315e001711f923fe7
nownabe <nownabe@gmail.com>
2016-12-10 22:29:00 +0900

    :green_heart: Introduce .env to separate firebase projects for development
    Close #5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require("webpack")
+require("dotenv").config()
 
 module.exports = {
   context: __dirname + "/src",
@@ -10,7 +11,16 @@ module.exports = {
     path: __dirname + "/public",
     filename: "bundle.js"
   },
-  plugins: [new webpack.ProvidePlugin({ riot: "riot" })],
+  plugins: [
+    new webpack.ProvidePlugin({ riot: "riot" }),
+    new webpack.DefinePlugin({
+      __FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+      __FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      __FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      __FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      __FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+    })
+  ],
   devtool: "inline-source-map",
   module: {
     preLoaders: [
@@ -23,6 +33,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: "babel-loader"
       },
       {
